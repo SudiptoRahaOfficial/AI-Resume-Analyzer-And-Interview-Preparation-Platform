edd6e7ed12ab19001fbb2ecbb20947bc692d102e
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-25 15:37:13 +0600

    maked schema & model of otp & session

---
+++ src/models/otp.model.js
@@ -0,0 +1,44 @@
+/*
+	- file name: otp.model.js
+	- responsibility: responsible for OTP schema & model design
+ */
+
+// importing dependencis
+const { Schema, model } = require('mongoose')
+
+// making schema
+const otpSchema = new Schema(
+	{
+		email: {
+			type: String,
+			required: [true, 'Email is required'],
+		},
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'user',
+			required: [true, 'User is required'],
+		},
+		otpHash: {
+			type: String,
+			required: [true, 'OTP hash is required'],
+		},
+		attempts: {
+			type: Number,
+			default: 0,
+			min: [0, 'OTP attempts cannot be negative'],
+			max: [3, 'OTP attempts cannot exceed 3'],
+		},
+		expiresAt: {
+			type: Date,
+			required: [true, 'OTP expiration time is required'],
+			expires: 0,
+		},
+	},
+	{ timestamps: true },
+)
+
+// making model
+const otpModel = model('otp', otpSchema)
+
+// exporting model
+module.exports = otpModel
