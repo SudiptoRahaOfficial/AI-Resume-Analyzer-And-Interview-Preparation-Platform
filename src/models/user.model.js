5ac2e26ff4a37a6fdd04fff73f7ab8ab53c33c64
Sudipto Raha Official <sudiptoraha.official@gmail.com>
2026-09-25 09:21:05 +0600

    maked user schema & model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,7 +9,23 @@ const { Schema, model } = require('mongoose')
 // making schema
 const userSchema = new Schema(
 	{
-		
+		username: {
+			type: String,
+			trim: true,
+			required: [true, 'Username is required'],
+			unique: [true, 'This username is not available'],
+			maxlength: 15,
+		},
+		email: {
+			type: String,
+			trim: true,
+			required: [true, 'Email is required'],
+			unique: [true, 'Already have an account with this email'],
+		},
+		password: {
+			type: String,
+			required: [true, 'Password is required'],
+		},
 	},
 	{ timestamps: true },
 )
